a7e01ade18445501cbb0e15d5b2a3a27331a9dcc
Shun-Cheng Wu <shuncheng.wu91@gmail.co>
2023-04-07 09:27:51 +0200

    support auto compile assimp

--- cmake/UseAssimp.cmake
+++ cmake/UseAssimp.cmake
@@ -1,9 +1,14 @@
-
-find_package(Assimp QUIET)
+# Use 4.1.0 to prevent this error: https://github.com/assimp/assimp/issues/2754
+find_package(Assimp 4.1.0 QUIET)
 OPTION(WITH_ASSIMP "Build with Assimp support?" ${Assimp_FOUND})
 
 IF(WITH_ASSIMP)
     MESSAGE(STATUS "WITH Assimp")
-    find_package(Assimp 4.1.0 REQUIRED) # To prevent this error https://github.com/assimp/assimp/issues/2754
+    IF (NOT Assimp_FOUND)
+      INCLUDE(ConfigureTimeDependency)
+      add_configure_time_dependency(Assimp)
+      find_package(Assimp 4.1.0 REQUIRED)
+    ENDIF()
+    
     INCLUDE_DIRECTORIES(${Assimp_INCLUDE_DIRS})
 ENDIF()
